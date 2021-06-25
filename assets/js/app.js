700d97467829d5afb50fcd81b3816334d82c2506
Mohammed Dilkush MP <62303291+dilkushmp@users.noreply.github.com>
2021-06-25 22:52:15 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -133,13 +133,12 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
-                <form>
                   <form action="https://formcarry.com/s/rwVKh2kU6fA" method="POST" accept-charset="UTF-8" >
                   <input type="text" name="field1" placeholder="Your Name" required/>
                   <input type="email" name="field2" placeholder="Email Address"  required/>
                   <textarea name="field3" placeholder="Type your Message" required></textarea>
                   <input type="submit" value="Send"/>
-                </form>
+                  </form>
              </div>
           </div>
         </div>
